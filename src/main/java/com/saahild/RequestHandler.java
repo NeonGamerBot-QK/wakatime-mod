3b92d82f373da3d885a6556a5ade5a50a1f985bc
Saahil <neon@saahild.com>
2024-10-13 16:07:05 -0400

    feat: request handler

---
+++ src/main/java/com/saahild/RequestHandler.java
@@ -0,0 +1,20 @@
+package com.saahild;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public class RequestHandler {
+    public static void sendHeartbeat(String apiKey, String projectName, String serverURL) throws URISyntaxException {
+        var client = HttpClient.newHttpClient();
+
+        var request = HttpRequest.newBuilder(
+                new URI(serverURL + "/users/current/heartbeats"))
+                        .header("Content-Type", "application/json")
+                        .header("Authorization", "Basic " + apiKey).build();
+
+        client.sendAsync(request, HttpResponse.BodyHandlers.ofString());
+    }
+}
